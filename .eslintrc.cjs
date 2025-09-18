34181992e51d9d3cb1a99394551804b5c72368a8
zzangmin2 <sharon7224@gmail.com>
2025-09-18 13:58:26 +0900

    chore: 프로젝트 초기 설정 및 의존성 설치

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['react', '@typescript-eslint'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off', // React 17+는 자동 import라 필요 없음
+  },
+}
